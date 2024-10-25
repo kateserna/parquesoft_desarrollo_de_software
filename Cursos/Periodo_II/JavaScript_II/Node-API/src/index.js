2e0c3c9a972ed5c48a07962c32e6757b9b78d55b
ksernag <kserna.g@gmail.com>
2024-10-25 01:14:57 -0500

    acceso a la base de datos de mongoDB

--- Cursos/Periodo_II/JavaScript_II/Node-API/src/index.js
+++ Cursos/Periodo_II/JavaScript_II/Node-API/src/index.js
@@ -1,19 +1,29 @@
-// llama el paquete de express previmente instalado
+// llama el paquete de express previamente instalado
 const express = require("express"); 
 const app = express(); //instancia de express para acceder a sus metodos.
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+
+//Midleware
 app.use(express.json()); // permite usar middleware para que la aplicacion sepa que todo lo q va a compartir y tanto al recibir o enviar va a ser en formato json
+app.use(cors());// para manejar origenes cruzados. 
 
-//simulacion de base de datos
-const libros = [
-    {id:1, titulo: "1984", autor: "George Orwell"},
-    {id:2, titulo: "Cien años de soledad", autor: "Gabriel García Marquez"},
-];
+const mongoUri = process.env.MONGODB_URI;
 
-// ruta para mostrar listado de libros
+//conexion a la base de datos usando mongoose
+try {
+    mongoose.connect(mongoUri);
+    console.log("Conectado a MongoDB")
+} catch (error) {
+    console.error("Error de conexion", error) 
+}
+
+/*// ruta para mostrar listado de libros
 app.get("/libros", (req, res) =>{
     res.json(libros)
-});
+});*/
 
 // Crear el servidor
 app.listen(3000, () => {
