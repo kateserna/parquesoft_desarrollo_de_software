9bd58d042e89ea25ef9c48eaa653075beb781165
ksernag <kserna.g@gmail.com>
2024-10-26 11:42:14 -0500

    se termina request para guardar libros en base de datos y se crea la API para traer el listado de todos los libros

--- Cursos/Periodo_II/JavaScript_II/Node-API/src/index.js
+++ Cursos/Periodo_II/JavaScript_II/Node-API/src/index.js
@@ -30,19 +30,29 @@ const Libro = mongoose.model("Libro", libroSchema);
 
 //Rutas de la API
 //Ruta para crear un nuevo libro: post (porque vamos a necesitar datos)
-app.post("libros", async(req, res)=>{
+app.post("/libros", async(req, res)=>{
     const libro = new Libro({
         titulo: req.body.titulo,
         autor: req.body.autor, 
     })
-
+    //guardar el libro en la base de datos con metodo mongoose
     try {
-        
+        await libro.save();
+        res.json(libro);//respuesta al usuario en formato json
     } catch (error) {
-        
+        res.status(500).send("Error al guardar el libro");
+    }
+});
+
+//Traer el listado de todos los libros
+app.get("/libros", async(req, res)=>{
+    try {
+        const libros = await Libro.find();
+        res.json(libros);
+    } catch (error) {
+        res.status(500).send("Error al obtener los libros");
     }
 })
-
 // Crear el servidor
 app.listen(3000, () => {
     console.log("Servidor ejecutandose en http://localhost:3000");
