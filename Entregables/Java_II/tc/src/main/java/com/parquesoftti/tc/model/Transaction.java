1322e8fa8fdbcb2a264d11a537b9e439972d4b29
ksernag <kserna.g@gmail.com>
2024-11-09 01:21:33 -0500

    model transaction

---
+++ Entregables/Java_II/tc/src/main/java/com/parquesoftti/tc/model/Transaction.java
@@ -0,0 +1,38 @@
+package com.parquesoftti.tc.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "credit_card_id")
+    private CreditCard creditCard;
+
+
+}
