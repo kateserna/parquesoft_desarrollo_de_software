a70e227a3e42c3ba407b01785ee14050fded17b0
ksernag <kserna.g@gmail.com>
2024-11-13 17:35:51 -0500

    funcion input con estados de contador y mensaje

--- Entregables/React_I/appreact01/src/componentes/input.jsx
+++ Entregables/React_I/appreact01/src/componentes/input.jsx
@@ -3,10 +3,24 @@ import { Link } from "react-router-dom";
 
 export default function Input(){
     const [mensaje, setMensaje] = useState("");
+    const [contador, setContador] = useState(0);
+    const handleClick = () => {
+        setContador(contador + 1);
+    }
+
 
     return(
         <div>
+            <h1>Clicker</h1>
+            <button onClick={handleClick}>Click</button>
+            <p>Contador: {contador}</p>
             <h3>Ingrese un mensaje</h3>
+            <input 
+                type="text" 
+                value={mensaje}
+                placeholder="Escribe un mensaje"
+                onChange={evento => {setMensaje(evento.target.value)}}
+            />
             <p>{mensaje}</p>
             <h3>Retornar al Home</h3>
             <Link to="/">Inicio</Link>
