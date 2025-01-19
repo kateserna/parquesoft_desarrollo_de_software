13c85bc4939f7f1076a5aa18e595004512470c1c
ksernag <kserna.g@gmail.com>
2025-01-19 12:44:25 -0500

    update calculadora

--- Entregables/Python_I/Proyectos/Calculadora.py
+++ Entregables/Python_I/Proyectos/Calculadora.py
@@ -10,14 +10,14 @@ def multiplicacion (a, b):
 
 def division (a, b):
     if b != 0:
-        return a / b
+        return f'Resultado de la división es: {a / b}'
     else:
         return "Error: División por cero"
 
 #Mostrar menu:
 
 def mostrar_menu():
-    print("Calculadora Básica: ")
+    print("\nCalculadora Básica: ")
     print("1. Suma")
     print("2. Resta")
     print("3. Multiplicación")
@@ -31,19 +31,25 @@ while True:
         print("Saliendo del programa")
         break
 
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
-
-
-    if opcion in ["1", "2", "3", "4"]:
-        if opcion == "1":
-            print(f'Resultado de la suma es: {suma(num1, num2)}')
-        elif opcion == "2":
-            print(f'Resultado de la resta es: {resta(num1, num2)}')
-        if opcion == "3":
-            print(f'Resultado de la multiplicación es: {multiplicacion(num1, num2)}')
-        if opcion == "4":
-            print(f'Resultado de la división es: {division(num1, num2)}')
-    else:
+    if opcion not in ["1", "2", "3", "4"]:
         print("Opción no válida, intentar de nuevo. ")
+        continue
+
+    while True:
+        try:
+            num1 = float(input("Ingrese el primer número: "))
+            num2 = float(input("Ingrese el segundo número: "))
+            break
+        except ValueError:
+            print("Error: Entrada no válida. Digite un número.")
+            continue
+
+    if opcion == "1":
+        print(f'Resultado de la suma es: {suma(num1, num2)}')
+    elif opcion == "2":
+        print(f'Resultado de la resta es: {resta(num1, num2)}')
+    elif opcion == "3":
+        print(f'Resultado de la multiplicación es: {multiplicacion(num1, num2)}')
+    elif opcion == "4":
+        print(f'{division(num1, num2)}')
 
